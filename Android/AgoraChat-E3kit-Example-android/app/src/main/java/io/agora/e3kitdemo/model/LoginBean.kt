13074811726464b5db7768f0e9b95892c5fc267b
winskyan <winskyan_w@163.com>
2022-07-01 10:37:32 +0800

    add register and update sdk version

--- Android/AgoraChat-E3kit-Example-android/app/src/main/java/io/agora/e3kitdemo/model/LoginBean.kt
+++ Android/AgoraChat-E3kit-Example-android/app/src/main/java/io/agora/e3kitdemo/model/LoginBean.kt
@@ -2,5 +2,6 @@ package io.agora.e3kitdemo.model
 
 class LoginBean {
     var accessToken: String? = null
-    var userNickname: String? = null
+    var username: String? = null
+    var agoraUid: Int? = 0
 }
